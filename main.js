32579621c7811394d95848e37318a56772c00472
cuong <cuongzip2k6@gmail.com>
2024-07-29 22:10:59 +0700

    update

--- main.js
+++ main.js
@@ -1,76 +1,134 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+let restDurationElements = $$(".alarm-left-rest-duration");
+const modalElement = $(".modal");
+
+const modalInnerElement = $(".modal-inner");
+
+const listInputElements = modalInnerElement.querySelectorAll("input");
+
+const setTimeBtnElement = $(".modal-inner-btn-set");
+
+const countDownTimeElement = $(".count-down time");
+
+const startBtnElement = $(".count-down-start");
+const stopBtnElement = $(".count-down-stop");
+const resetBtnElement = $(".count-down-reset");
+const startAgainBtnElement = $(".count-down-start-again");
+
+const audioElement = $("#audio");
+
+const listAudiosElement = $(".list-audios");
+
+const bellElement = $(".bell");
+
+const addAlarmElement = $(".button-add-alarm");
+
+const listAlarmElement = $(".list-alarm");
+
 const app = {
+  listAlarms: [],
   addEventListener() {
-    const modalElement = $(".modal");
-    const modalInnerElement = $(".modal-inner");
+    let action = "";
+    let timeCountdown = {};
+    let totalSeconds = 0;
+    let indexActiveAlarm;
 
-    const setTimeBtnElement = $(".modal-inner-btn-set");
+    listAlarmElement.onclick = (e) => {
+      const buttonSlideElement = e.target.closest(".button-slide");
+      const alarmElement = e.target.closest(".alarm");
+      const index = alarmElement?.getAttribute("index");
 
-    const countDownTimeElement = $(".count-down time");
-    const startBtnElement = $(".count-down-start");
-    const stopBtnElement = $(".count-down-stop");
-    const resetBtnElement = $(".count-down-reset");
-    const startAgainBtnElement = $(".count-down-start-again");
+      if (buttonSlideElement) {
+        buttonSlideElement.classList.toggle("button-slide-active");
+        this.listAlarms[index].state = !this.listAlarms[index].state;
+        restDurationElements[index].innerText = "";
+      }
+      if (alarmElement && !buttonSlideElement) {
+        modalElement.classList.add("show");
+        const time = this.handleFormatTime(this.listAlarms[index].time);
+        for (let inputElement of listInputElements) {
+          inputElement.value = time[inputElement.id];
+        }
+        indexActiveAlarm = index;
+        action = "change-alarm";
+      }
+    };
+    addAlarmElement.onclick = () => {
+      modalElement.classList.add("show");
+      for (let inputElement of listInputElements) {
+        inputElement.value = "00";
+      }
+      action = "add-alarm";
+    };
 
-    const listInputElements = modalInnerElement.querySelectorAll("input");
-
-    const audioElement = $("#audio");
-
-    const listAudiosElement = $(".list-audios");
-
-    const bellElement = $(".bell");
-
-    let isCountdown = true;
-    let timeCountDownOrigin;
-    let timeCountDown;
+    window.onunload = () => {
+      localStorage.setItem("timeCountdown", JSON.stringify(timeCountdown));
+      localStorage.setItem("audioSrc", listAudiosElement.value);
+      localStorage.setItem("listAlarms", JSON.stringify(this.listAlarms));
+    };
 
     window.onload = () => {
       const audioSrc =
         localStorage.getItem("audioSrc") ||
         "./audio/day-di-le-duong-bao-lam-sieu-hai-nhacchuongviet.com.mp3";
+
       audioElement.src = audioSrc;
       listAudiosElement.value = audioSrc;
-      const data = JSON.parse(localStorage.getItem("timeCountDownOrigin"));
 
-      timeCountDownOrigin = data || {
+      this.listAlarms = JSON.parse(localStorage.getItem("listAlarms")) || [];
+
+      this.renderAlarms();
+
+      timeCountdown = JSON.parse(localStorage.getItem("timeCountdown")) || {
         hours: 0,
         minutes: 0,
         seconds: 0,
       };
-      timeCountDown = { ...timeCountDownOrigin };
-      let timeText = this.handleTime(timeCountDownOrigin).split(":");
-      let index = 0;
+
+      totalSeconds = this.convertTimeToTotalSeconds(timeCountdown);
+      let timeText = this.handleFormatTime(timeCountdown);
+
       for (let inputElement of listInputElements) {
         const activeClass = "modal-inner-time-group-menu-item-active";
         const menuElement = inputElement.nextElementSibling;
 
         const listMenuItemElements = menuElement.querySelectorAll("li");
 
+        let value = timeText[inputElement.id];
+
         for (let menuItemElement of listMenuItemElements) {
-          if (menuItemElement.getAttribute("value") === timeText[index]) {
+          if (menuItemElement.getAttribute("value") === value) {
             menuItemElement.classList.add(activeClass);
             break;
           }
         }
-        inputElement.value = timeText[index];
+
+        inputElement.value = value;
 
         inputElement.onfocus = () => {
           inputElement.select();
         };
 
         inputElement.onblur = () => {
-          if (!inputElement.value) inputElement.value = "00";
-        };
-
-        inputElement.oninput = () => {
-          if (Number(inputElement.value) > 60) {
-            inputElement.value = "60";
+          const valueNumber = Number(inputElement.value);
+          if (valueNumber >= 0 && valueNumber <= 9) {
+            inputElement.value = "0" + valueNumber;
           }
         };
 
-        index++;
+        inputElement.oninput = () => {
+          const valueNumber = Number(inputElement.value);
+          if (isNaN(valueNumber)) {
+            inputElement.value = value;
+          } else {
+            if (valueNumber > 60) {
+              inputElement.value = "60";
+            }
+            value = inputElement.value;
+          }
+        };
 
         menuElement.onmousedown = (e) => {
           if (
@@ -86,8 +144,43 @@ const app = {
           }
         };
       }
-      timeText = timeText.join(":");
-      countDownTimeElement.innerText = timeText;
+      countDownTimeElement.innerText = `${timeText.hours}:${timeText.minutes}:${timeText.seconds}`;
+
+      setInterval(() => {
+        const date = new Date();
+        isFireAlarm = false;
+        let index = 0;
+        for (let alarm of this.listAlarms) {
+          if (alarm.state) {
+            let totalSecondsAlarm = this.convertTimeToTotalSeconds(alarm.time);
+
+            let totalSecondSTimeNow =
+              (date.getHours() * 60 + date.getMinutes()) * 60 +
+              date.getSeconds();
+
+            let restDuration = totalSecondsAlarm - totalSecondSTimeNow;
+
+            if (restDuration < 0) {
+              restDuration += 24 * 60 * 60;
+            }
+            const time = this.convertTotalSecondsToTime(restDuration);
+
+            restDurationElements[
+              index
+            ].innerText = `Báo thức sao ${time.hours} giờ ${time.minutes} phút`;
+
+            if (totalSecondsAlarm === totalSecondSTimeNow) {
+              isFireAlarm = true;
+              alarm.state = false;
+              this.renderAlarms();
+            }
+          }
+          index++;
+        }
+        if (isFireAlarm) {
+          handleStartAudio();
+        }
+      }, 1000);
     };
 
     modalElement.onmousedown = () => {
@@ -97,35 +190,31 @@ const app = {
     modalInnerElement.onmousedown = (e) => {
       e.stopPropagation();
     };
+
     countDownTimeElement.onclick = () => {
-      isCountdown = true;
+      action = "set-count-down";
       modalElement.classList.add("show");
+      const timeText = this.handleFormatTime(timeCountdown);
+      for (let inputElement of listInputElements) {
+        inputElement.value = timeText[inputElement.id];
+      }
     };
+
     let countDownIntervalId;
     startBtnElement.onclick = () => {
+      handleStopAudio();
       countDownIntervalId = setInterval(() => {
-        if (
-          timeCountDown.seconds + timeCountDown.minutes + timeCountDown.hours >
-          0
-        ) {
-          if (--timeCountDown.seconds < 0) {
-            timeCountDown.seconds = 59;
-            if (--timeCountDown.minutes < 0) {
-              timeCountDown.minutes = 59;
-              if (--timeCountDown.hours < 0) {
-                timeCountDown.hours = 0;
-              }
-            }
-          }
-        } else {
+        if (--totalSeconds <= 0) {
           clearInterval(countDownIntervalId);
           stopBtnElement.classList.remove("show");
           startAgainBtnElement.classList.add("show");
           audioElement.currentTime = 0;
-          audioElement.play();
-          bellElement.classList.add("animation-shaking");
+          handleStartAudio();
+        } else {
+          countDownTimeElement.innerText = this.convertTimeToText(
+            this.convertTotalSecondsToTime(totalSeconds)
+          );
         }
-        countDownTimeElement.innerText = this.handleTime(timeCountDown);
       }, 1000);
       startBtnElement.classList.remove("show");
       stopBtnElement.classList.add("show");
@@ -140,18 +229,17 @@ const app = {
     };
 
     resetBtnElement.onclick = () => {
-      timeCountDown = { ...timeCountDownOrigin };
-      countDownTimeElement.innerText = this.handleTime(timeCountDownOrigin);
+      totalSeconds = this.convertTimeToTotalSeconds(timeCountdown);
+      countDownTimeElement.innerText = this.convertTimeToText(timeCountdown);
       resetBtnElement.classList.remove("show");
     };
 
     startAgainBtnElement.onclick = () => {
       stopBtnElement.classList.add("show");
       startAgainBtnElement.classList.remove("show");
-      timeCountDown = { ...timeCountDownOrigin };
-      countDownTimeElement.innerText = this.handleTime(timeCountDown);
-      audioElement.pause();
-      bellElement.classList.remove("animation-shaking");
+      totalSeconds = this.convertTimeToTotalSeconds(timeCountdown);
+      countDownTimeElement.innerText = this.convertTimeToText(timeCountdown);
+      handleStopAudio();
       startBtnElement.dispatchEvent(new Event("click"));
     };
 
@@ -160,64 +248,117 @@ const app = {
       for (let inputElement of listInputElements) {
         time[inputElement.id] = Number(inputElement.value);
       }
-      if (true) {
-        timeCountDownOrigin = time;
-        timeCountDown = { ...timeCountDownOrigin };
-        localStorage.setItem(
-          "timeCountDownOrigin",
-          JSON.stringify(timeCountDownOrigin)
-        );
-        countDownTimeElement.innerText = this.handleTime(timeCountDownOrigin);
-      } else {
+      handleStopAudio();
+      switch (action) {
+        case "set-count-down":
+          timeCountdown = time;
+          totalSeconds = this.convertTimeToTotalSeconds(timeCountdown);
+          countDownTimeElement.innerText =
+            this.convertTimeToText(timeCountdown);
+          stopBtnElement.classList.remove("show");
+          startBtnElement.classList.add("show");
+          resetBtnElement.classList.remove("show");
+          startAgainBtnElement.classList.remove("show");
+          if (totalSeconds <= 0) {
+            startBtnElement.disabled = true;
+          } else {
+            startBtnElement.disabled = false;
+          }
+          break;
+        case "add-alarm":
+          this.listAlarms.unshift({
+            time,
+            state: true,
+          });
+          this.renderAlarms();
+          break;
+        case "change-alarm":
+          this.listAlarms[indexActiveAlarm].time = time;
+          this.renderAlarms();
+          break;
       }
       modalElement.classList.remove("show");
+      resetBtnElement.classList.remove("show");
     };
     listAudiosElement.onchange = () => {
       audioElement.src = listAudiosElement.value;
-      localStorage.setItem("audioSrc", listAudiosElement.value);
       bellElement.classList.remove("animation-shaking");
     };
 
     bellElement.onclick = () => {
       if (bellElement.classList.contains("animation-shaking")) {
-        bellElement.classList.remove("animation-shaking");
-        audioElement.pause();
+        handleStopAudio();
       } else {
-        bellElement.classList.add("animation-shaking");
-        audioElement.play();
+        handleStartAudio();
       }
+    };
+
+    const handleStopAudio = () => {
+      bellElement.classList.remove("animation-shaking");
+      audioElement.pause();
+    };
+    const handleStartAudio = () => {
+      bellElement.classList.add("animation-shaking");
+      audioElement.play();
     };
   },
   handleTimeNow() {
     const timeNowElement = $(".time-now");
-
     setInterval(() => {
       const date = new Date();
-
-      const hours = String(date.getHours()).padStart(2, "0");
-      const minutes = String(date.getMinutes()).padStart(2, "0");
-      const seconds = String(date.getSeconds()).padStart(2, "0");
-
-      timeNowElement.innerText = `${hours} : ${minutes} : ${seconds}`;
+      timeNowElement.innerText = date.toTimeString().slice(0, 8);
     }, 1000);
   },
-  handleSlideBtn() {
-    const btnSlideElements = $$(".button-slide");
-    for (let btnSlideElement of btnSlideElements)
-      btnSlideElement.onclick = () => {
-        btnSlideElement.classList.toggle("button-slide-active");
-      };
+
+  renderAlarms() {
+    let html = "";
+    this.listAlarms.forEach((alarm, index) => {
+      html += `<div class="alarm" index='${index}'>
+      <div class="alarm-left">
+       <time>${this.convertTimeToText(alarm.time)}</time>
+       <span class="alarm-left-rest-duration"></span>
+      </div>
+      <button class="button-slide ${alarm.state ? "button-slide-active" : ""}">
+        <div class="button-slide-circle"></div>
+        <div class="button-slide-bg"></div>
+      </button>
+    </div>`;
+    });
+
+    listAlarmElement.innerHTML = html;
+
+    restDurationElements = $$(".alarm-left-rest-duration");
   },
-  handleTime(time) {
+  convertTotalSecondsToTime(totalSeconds) {
+    const hours = Math.floor(totalSeconds / 60 / 60);
+    const minutes = Math.floor((totalSeconds - hours * 60 * 60) / 60);
+    const seconds = totalSeconds - (hours * 60 * 60 + minutes * 60);
+    return {
+      hours,
+      minutes,
+      seconds,
+    };
+  },
+  convertTimeToTotalSeconds(time) {
+    return (time.hours * 60 + time.minutes) * 60 + time.seconds;
+  },
+  handleFormatTime(time) {
     const hours = String(time.hours).padStart(2, "0");
     const minutes = String(time.minutes).padStart(2, "0");
     const seconds = String(time.seconds).padStart(2, "0");
-    return `${hours}:${minutes}:${seconds}`;
+    return {
+      hours,
+      minutes,
+      seconds,
+    };
+  },
+  convertTimeToText(time) {
+    let newTime = this.handleFormatTime(time);
+    return `${newTime.hours}:${newTime.minutes}:${newTime.seconds}`;
   },
   start() {
     this.addEventListener();
     this.handleTimeNow();
-    this.handleSlideBtn();
   },
 };
 
